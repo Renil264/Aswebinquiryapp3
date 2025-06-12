ee9bec16067c2764c98d6be86b0f7bd83d1b1785
Renil <renil@e-softsys.com>
2025-06-12 18:46:48 +0530

    Notification.dart changes

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-
 class NotificationService {
   // ignore: unused_field
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
@@ -9,10 +8,26 @@ class NotificationService {
       FlutterLocalNotificationsPlugin();
 
   Future<void> init() async {
+    // Android initialization
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('app_icon');
-    final InitializationSettings initializationSettings =
-        InitializationSettings(android: initializationSettingsAndroid);
+    
+    // iOS initialization - THIS WAS MISSING!
+    const DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      requestCriticalPermission: false,
+      requestProvisionalPermission: false,
+    );
+
+    // Combined initialization settings
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS, // ← This was missing!
+    );
 
     await _flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
@@ -22,16 +37,17 @@ class NotificationService {
       },
     );
 
+    
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print('Got a message whilst in the foreground!');
       print('Message data: ${message.data}');
-
+    
       if (message.notification != null) {
         print('Message also contained a notification: ${message.notification}');
         _showNotification(message);
       }
     });
-
+    
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print('A new onMessageOpenedApp event was published!');
       _handleNotificationTap(message);
@@ -39,6 +55,7 @@ class NotificationService {
   }
 
   Future<void> _showNotification(RemoteMessage message) async {
+    // Android notification settings
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
         AndroidNotificationDetails(
       'your_channel_id',
@@ -48,15 +65,31 @@ class NotificationService {
       showWhen: false,
       icon: 'app_icon',
     );
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    // iOS notification settings - THIS WAS MISSING!
+    const DarwinNotificationDetails iOSPlatformChannelSpecifics =
+        DarwinNotificationDetails(
+      presentAlert: true,  // Show alert when app is in foreground
+      presentBadge: true,  // Update app badge
+      presentSound: true,  // Play sound
+      sound: 'default',    // Use default sound
+    );
+
+    // Combined notification settings
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics, // ← This was missing!
+    );
+
     await _flutterLocalNotificationsPlugin.show(
-      0,
-      message.notification?.title,
-      message.notification?.body,
+      DateTime.now().millisecondsSinceEpoch ~/ 1000, // Unique ID
+      message.notification?.title ?? 'New Message',
+      message.notification?.body ?? 'You have a new message',
       platformChannelSpecifics,
       payload: message.data['payload'],
     );
+
+    print('📱 Local notification shown for iOS/Android');
   }
 
   Future<void> _handleNotificationTap(RemoteMessage message) async {
@@ -65,6 +98,7 @@ class NotificationService {
   }
 
   void showNotification(RemoteMessage message) {
+    print('🔔 showNotification called for: ${message.notification?.title}');
     _showNotification(message);
   }
 }
