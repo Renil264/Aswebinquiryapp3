6338246d2502eafdf872cc673e2da17cd4fc2190
Renil <Renil@GESS.LOCAL>
2025-12-16 18:57:22 +0530

    Made changes in the IOS Notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,11 +10,10 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
+
     FirebaseApp.configure()
 
     UNUserNotificationCenter.current().delegate = self
-
     application.registerForRemoteNotifications()
 
     GeneratedPluginRegistrant.register(with: self)
